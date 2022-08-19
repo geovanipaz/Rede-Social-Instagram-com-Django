2ecfc0bf13d2d7d994806a1825082bf3b9aaa69a
Geovani Paz <geovanipaz7@gmail.com>
2022-08-19 10:31:03 -0300

    signup

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+import re
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Profile
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email já existe')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'Username já existe')
+                return redirect('signup')
+            else:
+                user = User.objects.create(
+                    username=username, email=email, password=password)
+                user.save()
+                #loga usuario e redireciona para pagina de configurações 
+                
+                #cria um objeto perfil para um novo usuario
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id
+                )
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request,'As senhas não são iguais')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
